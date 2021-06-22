1e0d3a2c8ef182ae34a1b5ba41826470c01b4458
MdTasib <ohidulalam442@gmail.com>
2021-06-22 10:49:50 +0600

    main navbar done

--- src/pages/Trending/Trending.js
+++ src/pages/Trending/Trending.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Trending = () => {
+    const [content, setContent] = useState([]);
+
+    const fetchTrending = async () => {
+        const { data } = await axios.get(`https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}`);
+        console.log(data);
+        setContent(data.results);
+    }
+
+    useEffect(() => {
+        fetchTrending();
+    }, [])
+
     return (
         <div>
             <span className='pageTitle'>Trending</span>
+            <div className="trending">
+                {
+
+                }
+            </div>
         </div>
     );
 };
