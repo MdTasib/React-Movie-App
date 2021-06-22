6b853d3d5276b3ee1b66867f7926ca89bf1affa6
MdTasib <ohidulalam442@gmail.com>
2021-06-22 18:04:21 +0600

    pagination done

--- src/components/Pagination/CustomPagination.js
+++ src/components/Pagination/CustomPagination.js
@@ -1,6 +1,6 @@
-import React from "react";
-import Pagination from "@material-ui/lab/Pagination";
-import { createMuiTheme, ThemeProvider } from "@material-ui/core";
+import React from 'react';
+import { Pagination } from '@material-ui/lab';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core';
 
 const darkTheme = createMuiTheme({
     palette: {
@@ -8,31 +8,25 @@ const darkTheme = createMuiTheme({
     },
 });
 
-export default function CustomPagination({ setPage, numOfPages = 10 }) {
-    // Scroll to top when page changes
+
+const CustomPagination = ({ setPage, numOfPages = 10 }) => {
     const handlePageChange = (page) => {
         setPage(page);
         window.scroll(0, 0);
-    };
+    }
 
     return (
-        <div
-            style={{
-                width: "100%",
-                display: "flex",
-                justifyContent: "center",
-                marginTop: 10,
-            }}
-        >
+        <div style={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "center",
+            marginTop: 10,
+        }}>
             <ThemeProvider theme={darkTheme}>
-                <Pagination
-                    onChange={(e) => handlePageChange(e.target.textContent)}
-                    count={numOfPages}
-                    color="primary"
-                    hideNextButton
-                    hidePrevButton
-                />
+                <Pagination count={numOfPages} onChange={(e) => handlePageChange(e.target.textContent)} />
             </ThemeProvider>
         </div>
     );
-}
+};
+
+export default CustomPagination;
